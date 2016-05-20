5980aadbc32e0e56e017fb22e995971723c26d81
maitreyoun <yannanginot@ypolygon.com>
2016-05-20 16:54:08 +0200

    upd css
    reverse old css, del reset.css

--- app/templates/layout.php
+++ app/templates/layout.php
@@ -9,7 +9,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
 	<!-- Appel de la CSS reset -->
-	<link rel="stylesheet" href="<?= $this->assetUrl('css/reset.css')?>">
+	<!-- <link rel="stylesheet" href="<?= $this->assetUrl('css/normalize.css')?>"> -->
 
 	<!-- Appel des CSS de datepicker -->
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/default.css')?>">
