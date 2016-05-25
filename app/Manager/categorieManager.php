82020b9675bed46f05cfa3a6c6e489f49b376be8
mkddc <malik.kaddache@gmail.com>
2016-05-25 13:40:07 +0200

    ajout style et gestion sous catégories

--- app/Manager/categorieManager.php
+++ app/Manager/categorieManager.php
@@ -5,5 +5,15 @@ namespace Manager;
 
 class CategorieManager extends \W\Manager\Manager {
 
+	// Fonction de récupération des sous catégories en fonction d'une catégorie parent :
+	public function get_sub_categories($id_categorie_parent){
+		$sql = "SELECT * FROM categories WHERE parent_id = :parent_id";
+
+		$stmt = $this->dbh->prepare($sql);
+		$stmt->bindValue(":parent_id", $id_categorie_parent);
+		$stmt->execute();
+
+		return $stmt->fetchAll();
+	}
 
 }
--- public/assets/js/form.js
+++ public/assets/js/form.js
@@ -63,7 +63,8 @@ $(document).ready(function() {
 						});
 					}
 				});
-			})
+			}
+		)
 	}
 
 	// Appel AJAX sur les catégories :
@@ -92,7 +93,7 @@ $(document).ready(function() {
 				{
 					//console.log("création div row");
 					// création d'une nouvelle div de classe radio
-					$divRadio = $('<div class="row">');
+					$divRow = $('<div class="row">');
 					// remise du compteur à zéro
 					$compteur = 0;
 				}
@@ -171,18 +172,88 @@ $(document).ready(function() {
 		console.log("complete");
 	});
 
+
+	// Gestion de la réaction au clic d'une catégorie parent :
 	// Gestion de l'encadré de la catégorie sélectionnée
-	$(document).on('click', 'input[type = radio]', function(event) {
+	$(document).on('click', 'input[name = "radCategorie"]', function(event) {
 		// Si il y a une catégorie de sélectionnée, on enlève son style :
 		$('input[type = radio]').parent().removeClass('focused');
 		// on cible la div parent du bouton radio cliqué
 		$categorieFocused = $('input[type = radio]:checked').parent();
 		// on ajoute le style :
 		$categorieFocused.addClass('focused');
+
+		// Récupération de l'id de la catégorie cliquée :
+		$id_categorie_parent = $('input[name = "radCategorie"]:checked').attr('id');
+
+		// lecture des sous-catégories :
+		$.ajax({
+			// TODO : A gerer dans les routes avec un paramètre id
+			url: '/api/subcategories/' + $id_categorie_parent,
+			type: 'GET',
+			dataType: 'json',
+			// data: {param1: 'value1'},
+		})
+		.done(function(json) {
+			console.log(json);
+			// Ajout de toutes les catégories parents dans un bloc
+			// qui apparaît en dessous
+			$elementSub = $('#rangeSubCategId');
+			// on le vide à cahque fois sinon cumulation des sous-catégories
+			$elementSub.empty();
+
+			// Création du label du select
+			$label_sub = $('<label for="selSubCatId" class="col-sm-2 control-label">Sous-type (facultatif)</label>');
+			$elementSub.append($label_sub);
+			// Création du select
+			$select_sub = $('<select id="selSubCatId" class="form-control" name="selSub" class="form-control">');
+			$option_no_select = $('<option selected disabled>');
+			$option_no_select.append("Sous-type d'événement (facultatif)");
+			$select_sub.append($option_no_select);
+
+			$col_sm_10 = $('<div class="col-sm-10">');
+			$elementSub.append($col_sm_10);
+
+			var compteur = 0;
+
+			// parcours des sous catégories :
+			$(json).each(function(index, el) {
+				compteur++;
+
+				$id_categorie_sub = $(json)[index]['id'];
+
+				// création des options du select
+				$option_sub = $('<option value = "' + $id_categorie_sub + '">')
+				$text_option_sub = $(json)[index]['libelle'];
+
+				$option_sub.append($text_option_sub);
+				$select_sub.append($option_sub);
+
+				// Ajout dans le bloc
+				$elementSub.append($select_sub);
+			});
+			$col_sm_10.append($select_sub);
+
+			if(compteur !== 0)
+			{
+				$elementSub.css('display', 'block');
+			}
+			else
+			{
+				$elementSub.css('display', 'none');
+			}
+
+
+		})
+		.fail(function(error) {
+			console.log(error);
+		})
+		.always(function() {
+			console.log("complete");
+		});
+
+
 	});
-
-
-
 
 
 
