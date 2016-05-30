51866db3384c768aed0bb2e6ac9fad2345bcc466
Quentin Soetemondt <Quentin Soetemondt>
2016-05-30 17:24:10 +0200

    CSS + correction mot de passe oublié

--- app/templates/default/oublie.php
+++ app/templates/default/oublie.php
@@ -4,22 +4,23 @@
 
 <?php $this->start('main_content') ?>
 
-<form method="POST">
-    <div class="row">
-        <div class="col-md-offset-2 col-md-3">
-                <label for="email">Email</label>
-                <?php if(isset($errors['email'])){
-                    echo $errors['email'];
-                }?>
+<form method="POST" id="formLogin">
+    <div class="row" class="mailOublierow" >
+        <div id="mailOublie">
+                <label class='labelForm' for="email">Email</label>
                 <input type="text" class="form-control" id="email" name="email" placeholder="Votr email">
+               
             </div>
         </div>
         <div class="row">
-        <div class="col-md-offset-5 col-md-1">
-        <div class="form-group">
-            <button type="submit" class="btn btn-primary" name="reinit">Réinitialise le mot de passe</button>
+            <div id="btnLogin">
+               <button type="submit" class="btn btn-primary" id="btnLog" name="reinit">Réinitialisez votre mot de passe.</button>
+            </div>
         </div>
-        </div>
+        <?php if(isset($errors['email'])){
+            echo "<p class='errorForm'>".$errors['email']."</p>";
+        }?>
     </div>
-
+ </form>
+ 
 <?php $this->stop('main_content') ?>
